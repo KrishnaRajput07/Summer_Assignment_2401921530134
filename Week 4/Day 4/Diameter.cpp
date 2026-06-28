2d3bd6fa4d2dcfbc03d04c0685551a4a0b1d0dca
Nonchalant21 <nonchalant21@huggingface.co>
2026-06-28 21:08:39 +0530

    Week-4

---
+++ Week 4/Day 4/Diameter.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+  class Solution {
+public:
+    int d = 0;
+    int helper(TreeNode* root){
+        if(!root) return 0;
+        int left = helper(root->left);
+        int right = helper(root->right);
+        d = max(d, left+right);
+        return 1+max(left, right);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root) return d;
+        int temp = helper(root);
+        return d;  
+    }
+};
